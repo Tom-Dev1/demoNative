a0d5ed10dfd77f5203b3f3dd7b25458ff83f7f8a
TruongThanh <truongthanhh88@gmail.com>
2024-05-23 16:21:17 +0700

    ccc

---
+++ components/Layouts/LittleLemonHearder.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+
+function LittleLemonHearder() {
+    return (
+        <View>
+            <Text>
+                WellCome to Little Lemon
+            </Text>
+        </View>
+    )
+}
+
+export default LittleLemonHearder
